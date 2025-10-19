b991c1942c446b9fb3c1ac930844859815ba0f05
Anastasiia <gorgona1597531597@gmail.com>
2025-10-19 14:40:09 +0200

    create currencyStore

--- lib/stores/currencyStore.ts
+++ lib/stores/currencyStore.ts
@@ -1,1 +1,32 @@
 // lib\stores\currencyStore.ts
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { getUserInfo as fetchCurrencyInfo } from "../service/opencagedataApi";
+
+type CurrencyState = {
+  baseCurrency: string;
+  setBaseCurrency: (currency: string) => void;
+    fetchAndSetCurrency: (latitude: number, longitud: number) => Promise<void>;
+};
+
+export const useCurrencyStore = create<CurrencyState>()(
+  persist(
+    (set) => ({
+      baseCurrency: "USD",
+
+      setBaseCurrency: (currency) => set({ baseCurrency: currency }),
+
+          fetchAndSetCurrency: async (latitude, longitude) => {
+        try {
+            const currency = await fetchCurrencyInfo({ latitude, longitude });
+          set({ baseCurrency: currency });
+        } catch (error) {
+          console.error("Failed to fetch user currency:", error);
+        }
+      },
+    }),
+    { name: "currency-storage" }
+  )
+);
+
+// const { baseCurrency, fetchAndSetCurrency } = useCurrencyStore(); для використання стану валюти
